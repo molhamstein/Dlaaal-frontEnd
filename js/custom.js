f1795a6464a968e6f35ca54a79f9bc5b69d5bf99
Molham Mahmoud <fatherboard1@gmail.com>
2018-01-07 12:05:52 +0200

    add paralax and menus to home

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,31 @@
 $( document ).ready(function() {
+	// initialize paroller.js 
+	$(window).paroller();
 	var Menu = $("#Menu");
 	var menuPosition = Menu.offset().top;
 	var currentScroll = $(window).scrollTop();
 	var ItemsContainer = $(".ItemsContainer");
+	var Jumbotron = $(".Jumbotron");
 	var Content = $(".Content");
+	var DropMenu = $(".DropMenu");
+	var MenuBtn = $(".TopMenu-item--iconDots");
+	var FilterMenuBtn = $(".ContentMenu-item--iconDots");
+	var NotificationMenu = $(".NotificationMenu");
+
+	Jumbotron.paroller({ factor: '-0.2', type: 'foreground', direction: 'vertical' });
+
+	MenuBtn.click(function(event) {
+		DropMenu.toggleClass('DropMenu--isShown');
+	});
+	FilterMenuBtn.click(function(event) {
+		DropMenu.toggleClass('DropMenu--isShown');
+	});
+	$(".TopMenu-item--iconBell").click(function(event) {
+		NotificationMenu.toggleClass('NotificationMenu--isShown');
+	});
+	$(".ContentMenu-item--iconBell").click(function(event) {
+		NotificationMenu.toggleClass('NotificationMenu--isShown');
+	});
 
 	$(window).resize(function() {
 		menuPosition = Menu.offset().top;
